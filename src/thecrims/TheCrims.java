509cf2f63b85349977083f90272d289010a540b3
Pivoman <mara.pivo@seznam.cz>
2016-04-27 01:17:41 +0200

    úpravy, nyní přidává respekt postupně svému klubu - ještě se poupraví

--- src/thecrims/TheCrims.java
+++ src/thecrims/TheCrims.java
@@ -20,24 +20,20 @@ public class TheCrims {
     
     public static void robbery(WebDriver driver) throws InterruptedException{
         driver.findElement(By.xpath("//span[.='Zlodějina']")).click();
+        Thread.sleep(1000);
         driver.findElement(By.id("single-robbery-select")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//option[.='Klubík - 25% D: 10000 SP: 100%']")).click();
         Thread.sleep(500);
-        //driver.findElement(By.xpath(".='Místní parchanti - 25% D: 6000 SP: 100%'")).click();
-        for (int i = 0; i < 12; i++) {
-            driver.findElement(By.id("single-robbery-select")).sendKeys(Keys.ARROW_DOWN);
-        }
         driver.findElement(By.id("single-robbery-select")).sendKeys(Keys.ENTER);
         for (int i = 0; i < 4; i++) {
-            //waitForElement(driver.findElement(By.id("single-robbery-submit")), 2);
-            Thread.sleep(1500);
+            Thread.sleep(1000);
             driver.findElement(By.id("single-robbery-submit")).click();
         }
-        
-        
-                
     }
 
     public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Marek\\Documents\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.thecrims.com");
         driver.findElement(By.xpath("//input[@placeholder='Username']")).clear();
@@ -45,19 +41,40 @@ public class TheCrims {
         driver.findElement(By.xpath("//input[@placeholder='Password']")).clear();
         driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("03e43b44a3dc");
         driver.findElement(By.xpath("//button[.='Přihlaš se']")).click();
+        int i=0;
         while(true){
             if(Integer.parseInt(driver.findElement(By.xpath("//span[@ng-bind='user.stamina']")).getText()) < 100){
+                if(i % 2 == 0){
+                    Thread.sleep(1000);
+                    driver.findElement(By.xpath("//div[@id='menu-sprite-businesses']")).click();
+                    Thread.sleep(1000);
+                    driver.findElement(By.linkText("Pivomansland")).click();
+                    Thread.sleep(1000);
+                    int value = Integer.parseInt(driver.findElement(By.xpath("//td[.='Maximální respekt']/../td[2]/input")).getAttribute("value"));
+                    value += 190;
+                    driver.findElement(By.xpath("//td[.='Maximální respekt']/../td[2]/input")).clear();
+                    driver.findElement(By.xpath("//td[.='Maximální respekt']/../td[2]/input")).sendKeys(String.valueOf(value));
+                    Thread.sleep(1000);
+                    driver.findElement(By.xpath("//input[@value='OK']")).click();
+                }
+                Thread.sleep(1000);
                 driver.findElement(By.xpath("//div[@id='menu-nightlife']")).click();
+                Thread.sleep(1000);
                 driver.findElement(By.xpath("//td[.='Pivomansland']/../td[@align='right']/form/input[@value='Vstup']")).click();
-                waitForElement(driver.findElement(By.xpath("//td[.='Hašiš']/../td[2][@class='ng-binding']")), 5);
+                Thread.sleep(1000);
                 String kolikKopeString = driver.findElement(By.xpath("//td[.='Hašiš']/../td[2][@class='ng-binding']")).getText();
-                kolikKopeString = kolikKopeString.substring(0,4);
+                int kolikkolik = kolikKopeString.length();
+                kolikKopeString = kolikKopeString.substring(0, kolikkolik-1);
                 float kolikKope = Float.parseFloat(kolikKopeString);
                 int doplnitVydrz = 100-(Integer.parseInt(driver.findElement(By.xpath("//span[@ng-bind='user.stamina']")).getText()));
-                int koupit = (int) (doplnitVydrz/kolikKope);
+                int koupit = (int) ((doplnitVydrz/kolikKope)+1);
+                Thread.sleep(1000);
                 driver.findElement(By.xpath("//td[.='Hašiš']/../td[4]/form/input[@name='quantity']")).sendKeys(String.valueOf(koupit));
+                Thread.sleep(200);
                 driver.findElement(By.xpath("//td[.='Hašiš']/../td[4]/form/input[@type='submit']")).click();
+                Thread.sleep(500);
                 robbery(driver);
+                i++;
             }
             else{
                 robbery(driver);
